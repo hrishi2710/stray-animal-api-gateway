03d8d92aa535fbab9a4473c3f990dfaeac0f257f
hrishi2710 <hrishirules2710@gmail.com>
2020-10-10 15:56:35 +0530

    changes

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 9997;
 const reactUiDir = path.resolve(__dirname,'../stray-animal-frontend/build');
+console.log(__dirname);
+console.log(reactUiDir);
 
 app.use(express.static(reactUiDir));
 
