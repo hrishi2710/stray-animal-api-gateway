360510e6ad90a2b639243f32391adfd220cc7265
hrishi2710 <hrishirules2710@gmail.com>
2020-10-10 14:54:11 +0530

    An initial server

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 9997;
+const reactUiDir = path.resolve(__dirname,'../stray-animal-frontend/build');
+
+app.use(express.static(reactUiDir));
+
+app.all('*', (req, res, next) => {
+    res.sendFile('./index.html', {
+        root : reactUiDir
+    })
+})
+
+app.listen(port, () => {
+    console.log (`Server is up on port ${port}`);
+})
